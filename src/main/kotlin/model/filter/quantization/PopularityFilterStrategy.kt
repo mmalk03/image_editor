274b7ae60459d31d96a810f4388f1d1d60abe2d5
malkinskim <malkinskim@student.mini.pw.edu.pl>
2018-03-27 11:35:01 +0200

    Color quantization using popularity algorithm

--- src/main/kotlin/model/filter/quantization/PopularityFilterStrategy.kt
+++ src/main/kotlin/model/filter/quantization/PopularityFilterStrategy.kt
@@ -4,35 +4,50 @@ import javafx.scene.image.Image
 import javafx.scene.paint.Color
 import model.filter.FilterStrategy
 
-class PopularityFilterStrategy(val image: Image, colorPaletteSize: Int) : FilterStrategy() {
+class PopularityFilterStrategy(val image: Image, val colorPaletteSize: Int) : FilterStrategy() {
 
-    val colorPalette = getColorPalette(image, colorPaletteSize)
+    companion object {
+        const val RED_MULTIPLIER = 255 * 255
+        const val GREEN_MULTIPLIER = 255
+        const val BLUE_MULTIPLIER = 1
+    }
+
+    private lateinit var colorPalette: ArrayList<Color>
+
+    init {
+        initColorPalette()
+    }
 
     override fun getColor(image: Image, i: Int, j: Int): Color {
         return colorPalette.minBy { getDistanceBetweenColors(image.pixelReader.getColor(i, j), it) }!!
     }
 
-    private fun getColorPalette(image: Image, colorPaletteSize: Int): List<Color> {
-        //TODO create list of frequency of separate color, then Color.color(index, index % 256, index % 256*256)
-        val colorMap = HashMap<Color, Int>(256 * 256 * 256)
-        for (r in 0 until 256) {
-            for (g in 0 until 256) {
-                for (b in 0 until 256) {
-                    colorMap[Color.color(r.toDouble() / 256, g.toDouble() / 256, b.toDouble() / 256)] = 0
-                }
-            }
-        }
+    private fun initColorPalette() {
+        val colorList = IntArray(256 * 256 * 256)
 
+        colorPalette = ArrayList(colorPaletteSize)
         val pixelReader = image.pixelReader
         for (i in 0 until image.width.toInt()) {
             for (j in 0 until image.height.toInt()) {
-                colorMap[pixelReader.getColor(i, j)]?.inc()
+                colorList[pixelReader.getColor(i, j).let {
+                    it.red * RED_MULTIPLIER +
+                            it.green * GREEN_MULTIPLIER +
+                            it.blue * BLUE_MULTIPLIER
+                }.toInt()]++
             }
         }
+        (0 until colorPaletteSize).forEach {
+            val maxFrequencyColor = colorList.max()!!
+            val maxFrequencyColorIndex = colorList.indexOf(colorList.max()!!)
 
-        val sortedColorMap: Map<Color, Int> = colorMap.toList().sortedBy { pair -> pair.second }.toMap()
-        val colorsWithMaxFrequency = ArrayList<Color>(colorPaletteSize)
-        (0 until colorPaletteSize).mapTo(colorsWithMaxFrequency) { sortedColorMap.keys.elementAt(sortedColorMap.size - 1 - it) }
-        return colorsWithMaxFrequency
+            val red = (maxFrequencyColor / RED_MULTIPLIER)
+            val green = (maxFrequencyColor - red * RED_MULTIPLIER) / GREEN_MULTIPLIER
+            val blue = (maxFrequencyColor - red * RED_MULTIPLIER - green * GREEN_MULTIPLIER) / BLUE_MULTIPLIER
+            colorPalette.add(Color.color(
+                    (red / 255.0),
+                    (green / 255.0),
+                    (blue / 255.0)))
+            colorList[maxFrequencyColorIndex] = 0
+        }
     }
 }
